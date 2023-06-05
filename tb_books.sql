af0331776352f10a70b17d4fbfbb24ce505e8ba6
Emerson Lucas <94191948+EmersonLucas@users.noreply.github.com>
2023-06-05 05:51:28 -0300

    Add files via upload

---
+++ tb_books.sql
@@ -0,0 +1,7 @@
+insert into `tb_books` (`autor`, `editora`, `id`, `nome`, `quantidade`) values ('Virginia Woolf', 'L&PM', '1', 'Ao Farol (1927)', 32);
+insert into `tb_books` (`autor`, `editora`, `id`, `nome`, `quantidade`) values ('Herman Melville', 'XeroxLTD', '2', 'Moby Dick (1851)', 20);
+insert into `tb_books` (`autor`, `editora`, `id`, `nome`, `quantidade`) values ('Dante Alighieri', 'XeroxLTD', '3', 'A Divina Comédia (1304—1321)', 35);
+insert into `tb_books` (`autor`, `editora`, `id`, `nome`, `quantidade`) values ('Homero', 'ClassicosLTD', '4', 'Ilíada (séc. VIII a.C.)', 55);
+insert into `tb_books` (`autor`, `editora`, `id`, `nome`, `quantidade`) values ('Miguel de Cervantes', 'VolvmeLTD', '5', 'Dom Quixote', 60);
+insert into `tb_books` (`autor`, `editora`, `id`, `nome`, `quantidade`) values ('Vladimir Nabokov', 'O GLOBO', '6', 'Lolita (1955)', 70);
+insert into `tb_books` (`autor`, `editora`, `id`, `nome`, `quantidade`) values ('Homero', 'ClassicosLTD', '7', 'Odisseia', 23);
---
+++ tb_users.sql
@@ -0,0 +1,4 @@
+insert into `tb_users` (`blocked`, `cpf`, `employee`, `id`, `name`, `password`, `telephoneNumber`) values (0, '62373-000', 0, '1', 'ana', '123', '(88) 98245-0059');
+insert into `tb_users` (`blocked`, `cpf`, `employee`, `id`, `name`, `password`, `telephoneNumber`) values (0, '711.255.803-48', 0, '2', 'sergio', '123', '(88) 99515-5630');
+insert into `tb_users` (`blocked`, `cpf`, `employee`, `id`, `name`, `password`, `telephoneNumber`) values (0, '720.287.685-09', 0, '3', 'claudio', '123', '(71) 98836-0584');
+insert into `tb_users` (`blocked`, `cpf`, `employee`, `id`, `name`, `password`, `telephoneNumber`) values (0, '382.072.604-75', 1, '4', 'emerson', '123', '(82) 99345-2517');
